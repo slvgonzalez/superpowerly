4e3552b5c375c0e49ed02ae4077e1ea40e8d3c82
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-21 19:19:05 +0000

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_172351) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
