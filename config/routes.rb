e1ef191af96ebd0168d5ca4daa07a0c20ae8f65a
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-22 11:37:50 +0000

    devise controller and pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :superpowers, only: %i[index new create show] do
+    resources :bookings, only: %i[index new create]
+  end
 end
