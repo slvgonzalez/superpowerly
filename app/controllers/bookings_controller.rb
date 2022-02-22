a2e4d1a56f30cdf54875284ec9e37974238fa5f2
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-22 13:24:36 +0000

    bookings controller index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+  def index
+    if user_signed_in?
+      @user = current_user
+      @bookings = Booking.where(user: @user)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+  end
 end
