ae75eeab11737258d0c07ecfce49da2209c881fc
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-22 14:45:07 +0000

    Superpowers show

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -3,9 +3,26 @@ class SuperpowersController < ApplicationController
     @superpowers = Superpower.all
   end
 
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
   def new
+    @user = User.find(params[:user_id])
+    @superpower = Superpower.new
   end
 
   def create
+    @superpower = Superpower.new(superpower_params)
+    # we need `user_id` to associate superpower with corresponding user?
+    @user = User.find(params[:user_id])
+    @user.superpower = @superpower
+    @superpower.save!
+    redirect_to superpowers_path(@user)
+  end
+  private
+
+  def superpower_params
+    params.require(:superpower).permit(:name, :price, :description, :user_id)
   end
 end
