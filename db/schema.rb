b9c602e0b95fb5eca0e72514ad40eb8aff7a9b16
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-22 11:11:43 +0000

    booking model attributes and references

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_110719) do
+ActiveRecord::Schema.define(version: 2022_02_22_111056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "superpower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["superpower_id"], name: "index_bookings_on_superpower_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "superpowers", force: :cascade do |t|
     t.string "name"
@@ -40,5 +51,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_110719) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "superpowers"
+  add_foreign_key "bookings", "users"
   add_foreign_key "superpowers", "users"
 end
