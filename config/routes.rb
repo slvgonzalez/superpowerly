27668085541a18ce4ec06f9abd28b13f0a942f45
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-22 11:48:58 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :superpowers, only: %i[index new create show] do
-    resources :bookings, only: %i[index new create]
+    resources :bookings, only: %i[new create]
   end
+  resources :bookings, only: :index
 end
