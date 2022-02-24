5ebf5e71697db62e771c85b671d4a456de3a4f05
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-24 13:05:54 +0000

    pg_search gem

--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -2,8 +2,17 @@ class Superpower < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
-
   validates :description, presence: true
   validates :price, presence: true
   validates :name, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+                  against: %i[name description category],
+                  associated_against: {
+                    user: %i[username]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
